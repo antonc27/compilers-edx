bc01dbcce725f0de3ec656bda012a54e70ed6fd1
antonc27 <antonc27@mail.ru>
2020-11-21 18:37:08 +0100

    - Basic equality test program

--- PA5J/CgenTest.java
+++ PA5J/CgenTest.java
@@ -12,7 +12,8 @@ class CgenTest {
     @ParameterizedTest
     @ValueSource(strings = {
             "example.cl", "hello_world.cl", "two_args.cl", "basic_sum.cl", "basic_arith.cl",
-            "eval-order-arith.cl", "fact.cl", "simple_let.cl", "exp.cl", "bigexpr.cl", "basic-init.cl"
+            "eval-order-arith.cl", "fact.cl", "simple_let.cl", "exp.cl", "bigexpr.cl", "basic-init.cl",
+            "basicequality.cl"
     })
     public void testExample(String programFilename) {
         String programPath = "tests/" + programFilename;
