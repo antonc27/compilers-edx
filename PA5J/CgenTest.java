7de33c8106257895d47fa060c0dc1eccc01bd7ed
antonc27 <antonc27@mail.ru>
2020-11-22 13:55:35 +0100

    - Integer negation test program

--- PA5J/CgenTest.java
+++ PA5J/CgenTest.java
@@ -14,7 +14,7 @@ class CgenTest {
             "example.cl", "hello_world.cl", "two_args.cl", "basic_sum.cl", "basic_arith.cl",
             "eval-order-arith.cl", "fact.cl", "simple_let.cl", "exp.cl", "bigexpr.cl", "basic-init.cl",
             "basicequality.cl", "simple_new.cl", "calls.cl", "sequence.cl", "if_and_compare.cl",
-            "simple_while.cl"
+            "simple_while.cl", "neg.cl"
     })
     public void testExample(String programFilename) {
         String programPath = "tests/" + programFilename;
